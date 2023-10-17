6c5f94010bbf1ceabd9c75189511954ef2c7e904
kevin <kevindzinde2@gmail.com>
2023-10-17 15:25:53 +0200

    init

---
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,45 @@
+package com.in28minutes.microservices.currencyexchangeservice;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class CurrencyExchangeController {
+
+
+    private CurrencyExchangeRepository repository;
+
+    private Environment environment;
+    private Logger logger= LoggerFactory.getLogger(CurrencyExchangeController.class);
+
+    public CurrencyExchangeController(CurrencyExchangeRepository repository, Environment environment) {
+        this.repository = repository;
+        this.environment = environment;
+    }
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
+        logger.info("retrieveExchangeValue called with {} to {} ",from , to);
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from,to);
+        if(currencyExchange==null){
+            throw new  RuntimeException("Unable to FInd for "+ from +"to" + to);
+        }
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        //CHANGE-KUBERNETES
+        String host = environment.getProperty("HOSTNAME");
+        String version = "v3.3";
+
+        currencyExchange.setEnvironment(port + " " + version + " " + host);
+
+
+        return currencyExchange;
+
+    }
+}
